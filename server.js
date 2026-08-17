3af359e95008fb6f8af6e36160fd2bce026d64a4
Abhineet <abhineet.am13@gmail.com>
2026-08-17 23:55:01 +0530

    feat: implement authentication logic with register and login controllers and routes

--- server.js
+++ server.js
@@ -1,14 +1,10 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectDB = require('./src/database/db')
-const authRoutes = require('./src/routes/auth.routes')
-
 
 connectDB();
 
-app.use('/api/auth',authRoutes)
-
-
-app.listen(3000,()=>{
-    console.log("Server is runing at port 3000")
+app.listen(3000, () => {
+    console.log("Server is running at port 3000")
 })
+
--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,41 +3,72 @@ const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 
 
-async function registerUser (req,res){
-    const {firstName:{firstName,lastName}, email , password} = req.body;
+async function registerUser(req, res) {
+    const { email, fullName: {firstName , lastName} , password } = req.body;
 
-    const isUserAlreadyExist = await userModel.findOne({email}) 
+    const isUserAlreadyExist = await userModel.findOne({ email })
 
-    if(isUserAlreadyExist){
-        return res.status(400).json({message:"User already exists"})
+    if (isUserAlreadyExist) {
+        return res.status(400).json({ message: "User already exists" })
     }
 
-    const hashPassword = await bcrypt.hash(password,10)
+
+    const hashPassword = await bcrypt.hash(password, 10)
 
 
     const user = await userModel.create({
-        fullname:{
+        fullname: {
             firstName,
             lastName
         },
         email,
-        password : hashPassword
+        password: hashPassword
     })
 
-    const token = jwt.sign({ id: user._id},process.env.JWT_SECRET)
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
 
-    res.cookie("token",token)
+    res.cookie("token", token)
 
     res.status(201).json({
-        message:"User registered successfully",
-        user:{
-            email : user.email,
+        message: "User registered successfully",
+        user: {
+            email: user.email,
             _id: user._id,
             fullName: user.fullName
         }
-    
+
     })
-    
+
 }
 
-module.exports = {registerUser}
+async function loginUser(req, res) {
+    const { email, password } = req.body;
+
+    const user = await userModel.findOne({ email })
+
+    if (!user) {
+        return res.status(404).json({ message: "Invalid email or password" })
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password)
+
+    if (!isPasswordValid) {
+        return res.status(401).json({ message: "Invalid email or password" })
+    }
+
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+
+    res.cookie("token", token)
+
+    res.status(200).json({
+        message: "User logged in successfully",
+        user: {
+            email: user.email,
+            _id: user._id,
+            fullName: user.fullName
+        }
+
+    })
+
+}
+
+module.exports = { registerUser, loginUser }
