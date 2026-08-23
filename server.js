83ceb45dac332bcd6765f53fb162d8554e0f28fd
Abhineet <abhineet.am13@gmail.com>
2026-08-23 15:29:56 +0530

    feat: integrate socket.io into server to enable real-time communication capabilities

--- server.js
+++ server.js
@@ -1,10 +1,14 @@
 require("dotenv").config()
 const app = require("./src/app")
 const connectDB = require('./src/database/db')
+const initSocketServer = require('./src/Sockets/sockets.server')
+const httpServer = require("http").createServer(app);
 
 connectDB();
+initSocketServer(httpServer);
 
-app.listen(3000, () => {
+
+httpServer.listen(3000, () => {
     console.log("Server is running at port 3000")
 })
 
---
+++ src/Sockets/sockets.server.js
@@ -0,0 +1,14 @@
+const { Server } = require("socket.io");
+
+function initSocketServer(httpServer) {
+
+    const io = new Server(httpServer, {})
+
+    io.on("connection", (socket) => {
+        console.log("New Socket Connected", socket.id)
+    })
+}
+
+
+
+module.exports = initSocketServer;
